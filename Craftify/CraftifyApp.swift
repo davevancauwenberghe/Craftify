323e10fb669f9a7703782ab59905c4b57d14067a
“appledave”vc <davevancauwenberghe@outlook.com>
2025-02-07 03:28:04 +0100

    Initial Commit

---
+++ Craftify/CraftifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftifyApp.swift
+//  Craftify
+//
+//  Created by Dave Van Cauwenberghe on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CraftifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CraftifyTests/CraftifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CraftifyTests.swift
+//  CraftifyTests
+//
+//  Created by Dave Van Cauwenberghe on 07/02/2025.
+//
+
+import Testing
+@testable import Craftify
+
+struct CraftifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
