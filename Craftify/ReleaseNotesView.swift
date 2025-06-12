bd78a631c04ade475a7b534b7a8e6671c1dca6ca
“appledave”vc <davevancauwenberghe@outlook.com>
2025-06-12 03:49:41 +0200

    - Build number update

--- Craftify/ReleaseNotesView.swift
+++ Craftify/ReleaseNotesView.swift
@@ -77,7 +77,7 @@ struct ReleaseNote {
 }
 
 let releaseNotes: [ReleaseNote] = [
-    ReleaseNote(version: "Version 1.0 - Build 58-75", changes: [
+    ReleaseNote(version: "Version 1.0 - Build 58-76", changes: [
         "Major report recipes issues improvements",
         "Privacy policy & data control added",
         "Network status added",
